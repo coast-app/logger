6a003c79c2fc07e200b6fa646e50e960228dd5b4
Jeremy Shimko <jeremy.shimko@gmail.com>
2019-07-16 09:49:31 -0400

    add log format config

--- src/main.js
+++ src/main.js
@@ -13,10 +13,15 @@ if (!levels.includes(level)) {
   level = 'INFO';
 }
 
+// allow overriding the stdout log formatting
+// available options: short|long|simple|json|bunyan
+// https://www.npmjs.com/package/bunyan-format
+const outputMode = process.env.LOG_FORMAT || 'short';
+
 // default console config (stdout)
 const streams = [{
   level,
-  stream: bunyanFormat({ outputMode: 'short' })
+  stream: bunyanFormat({ outputMode })
 }];
 
 // create default logger instance
