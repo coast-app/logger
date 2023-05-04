cec5bfe021321f6b4e25a10126d3047de3ef869e
Russell Taga <russell@coastapp.com>
2023-05-04 09:48:16 -0700

    Include build files

---
+++ dist/logger.d.ts
@@ -0,0 +1,4 @@
+export declare const Logger: import("pino").Logger<{
+    level: string;
+    redact: string[];
+}>;
---
+++ dist/logger.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Logger = void 0;
+const pino_1 = __importDefault(require("pino"));
+const level = process.env.LOG_LEVEL;
+const redact = [
+    "MYSQL_PASSWORD",
+    "STRIPE_SECRETKEY",
+    "JWT_SECRET",
+    "headers.authorization",
+    "user.passcode",
+    "user.lastName",
+    "user.phoneNumber",
+    "user.email",
+];
+exports.Logger = (0, pino_1.default)({
+    level,
+    redact,
+});
